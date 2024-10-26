37eff69a2b4abeb1d8fedebf39b0645046e691d0
Ben Cooper <cooperben83@gmail.com>
2024-10-26 14:30:50 +0100

    added openAI transcription

---
+++ speech_to_text.py
@@ -0,0 +1,17 @@
+from utils.openai import client
+
+
+def get_transcription(file_path: str) -> str:
+    audio_file = open(file_path, "rb")
+    transcription = client.audio.transcriptions.create(
+        model="whisper-1", file=audio_file, response_format="text"
+    )
+    return transcription
+
+
+import os
+
+current_dir = os.path.dirname(__file__)
+file_path = os.path.join(current_dir, "Louis.m4a")
+text = get_transcription(file_path)
+print(text)
